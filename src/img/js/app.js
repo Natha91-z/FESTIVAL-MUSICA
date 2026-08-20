3a534ba3121625342d460989c906f23c19246486
Your Name <nathagpinilla@gmail.com>
2026-08-19 21:09:28 -0500

    Se realiza visualizacion en js y scss

--- src/img/js/app.js
+++ src/img/js/app.js
@@ -11,8 +11,51 @@ function crearGaleria() {
         const imagen = document.createElement('IMG')
         imagen.scr = 'src/img/gallery/full/${i}.jpg'
         imagen.alt = 'Imagen Galeria'
+
+        // Event Handler
+        imagen.onclick = function() {
+            mostrarImagen(1)
+        }
         
         galeria.appendChild(imagen)
     }
 
+}
+function mostrarImage(i) {
+    const imagen = document.createElement('IMG')
+    imagen.scr = 'src/img/gallery/full/${i}.jpg'
+    imagen.alt = 'Imagen Galeria'
+
+    // Generar Modal
+    const modal = document.createElement('DIV')
+    modal.classList.add('modal')
+    modal.onclick = cerrarModal
+
+    // Boton cerrar modal 
+    const cerrarModalBtn = document.createElement('BUTTON')
+    cerrarModalBtn.textContent = 'x'
+    cerrarModalBtn.classList.add('btn-cerrar')
+    cerrarModalBtn.onclick = cerrarModal
+
+    modal.appendChild(cerrarModalBtn)
+    modal.appendChild(imagen)
+
+    // agregar al HTML
+    const body = document.querySelector('body')
+    body.classList.add('overflow-hidden')
+    body.appendChild(modal)
+
+}
+
+function cerrarModal() {
+    const modal = document.querySelector('.modal')
+    modal.classList.add('fade-out')
+    
+    setTimeout(() => {
+        modal?.remove()
+
+        const body = document.querySelector('body')
+        body.classList.remove('overflow-hidden')
+    }, 500);
+    
 }
